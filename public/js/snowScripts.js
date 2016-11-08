d8e1968465cb5001be6432ddf41e0bc2a130862b
Talon Hughes <talon.hughes@gmail.com>
2016-11-08 12:27:29 -0700

    testing github.io

---
+++ public/js/snowScripts.js
@@ -0,0 +1,9 @@
+//SNOW ON THE SCREEN
+$(document).ready(
+    function () {
+        snowStorm.snowColor = '#DCE6E7';
+        snowStorm.flakesMaxActive = 50;
+        snowStorm.useTwinkleEffect = true;
+    }
+
+);
