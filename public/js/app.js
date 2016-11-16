4186365decafcd0a50634205e756059c3559d84b
Talon Hughes <talon.hughes@gmail.com>
2016-11-16 15:39:21 -0700

    more progress on the personalization view. need to add more options and personalized paragraphs. I also added the pagination on the ornaments

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('santaLettersApp', ['ui.router', 'ngAnimate']);
+var app = angular.module('santaLettersApp', ['ui.router']);
 
 app.config(function($urlRouterProvider, $stateProvider) {
 
--- public/js/controllers/letterCreateCtrl.js
+++ public/js/controllers/letterCreateCtrl.js
@@ -98,10 +98,45 @@ $scope.current = 'scLetterHead';
 
 //Personalized Letter Changes
 
-$scope.selectItemsFilterCriteria = [
-  {id: 1, name: "An event that occurred in the past 12 months"},
-  {id: 2,name: "child/family got a new dog"},
-  {id: 3, name: "child/family got a new cat"}
+$scope.selectItemsFilter = [
+  {id: 1, name: "Select an event that occurred in the past 12 months"},
+  {id: 2,name: "child/family got a new pet dog", para: "I see you got a pet dog this year. I'll be as quiet as a mouse and be sure not to wake your dog when I visit on Christmas Eve! I only ask that you teach him not to bite me as I come down the chimney!"},
+  {id: 3, name: "child/family got a new pet cat", para: "I see you got a new pet cat this year. I promise not to drink all the milk so your kitty can have some on Christmas morning! I can't say the same for the cookies though!"},
+  {id: 4, name: "child/family got a new pet rabbit", para: "I see you got a new pet rabbit this year. Do you think it's related to the Easter Bunny? He did told me at dinner once that many of his brothers and sisters wanted to grow up to be professional pet bunnies!"},
+  {id: 5, name: "Child learned to walk", para: "Mrs. Claus told me you have learned to walk! When you feel ready, feel free to walk over here to the North Pole and help me feed the reindeer and keep the elves in check."},
+  {id: 6, name: "Child lost their first tooth", para: "The tooth fairy told me he visted your house and took your first tooth! I hope he left a generous gift under your pillow. Make sure to keep smiling!"},
+  {id: 7, name: "Child became potty trained", para: "What great news! The elves just told me you are now potty trained. All of us here at the North Pole are so proud of you! Make sure to keep your hands clean by washing them after each time you go potty."},
+  {id: 8, name: "", para: ""},
+  {id: 9, name: "", para: ""},
+  {id: 10, name: "", para: ""},
+  {id: 11, name: "", para: ""},
+  {id: 12, name: "", para: ""},
+  {id: 13, name: "", para: ""},
+  {id: 14, name: "", para: ""},
+  {id: 15, name: "", para: ""},
+  {id: 16, name: "", para: ""},
+  {id: 17, name: "", para: ""},
+  {id: 18, name: "", para: ""},
+  {id: 19, name: "", para: ""},
+  {id: 20, name: "", para: ""},
+  {id: 21, name: "", para: ""},
+  {id: 22, name: "", para: ""},
+  {id: 23, name: "", para: ""},
+  {id: 24, name: "", para: ""},
+  {id: 25, name: "", para: ""},
+  {id: 26, name: "", para: ""},
+  {id: 27, name: "", para: ""},
+  {id: 28, name: "", para: ""},
+  {id: 29, name: "", para: ""},
+  {id: 30, name: "", para: ""},
+  {id: 31, name: "", para: ""},
+  {id: 32, name: "", para: ""},
+  {id: 33, name: "", para: ""},
+  {id: 34, name: "", para: ""},
+  {id: 35, name: "", para: ""},
+  {id: 36, name: "", para: ""},
+  {id: 37, name: "", para: ""},
+  {id: 38, name: "", para: ""}
 ];
 
 });
