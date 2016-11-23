93d4373b3d65a14ffc98d38ed16a546e48535077
Talon Hughes <talon.hughes@gmail.com>
2016-11-23 11:37:18 -0700

    fixed the api calls so they dont use the localhost url

--- public/js/services/letterCreateSrv.js
+++ public/js/services/letterCreateSrv.js
@@ -3,14 +3,14 @@ app.service('letterCreateSrv', function($http) {
     this.getAllProducts = function(id) {
         return $http({
             method: 'GET',
-            url: 'http://localhost:2000/api/products'
+            url: '/api/products'
         });
     };
 
     this.getProduct = function(id) {
         return $http({
           method: 'GET',
-          url: 'http://localhost:2000/api/products/'
+          url: '/api/products/'
         });
       };
 
