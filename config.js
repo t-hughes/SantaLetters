5832138c757f9db541d350b3bf6636dbdf502efb
Talon Hughes <talon.hughes@gmail.com>
2016-12-08 18:55:04 -0700

    added a hosted server

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'postgres://postgres:Dec170ct131989!@localhost/ecommerce',
+  database: 'postgres://cjdhhozc:y9uAHxhFf2933Ve1aNBLUPQG0DfWOga4@tantor.db.elephantsql.com:5432/cjdhhozc',
   // secret: ''
 };
